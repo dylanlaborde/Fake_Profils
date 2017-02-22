1067dded612b236a62b27a695f38e284ce711c4d
Dylan <dylan4047@gmail.com>
2017-02-22 14:04:26 +0100

    utilisation de Faker , return des donnée de faker dans la vue + les date pour l'affichage des date de naissance et de l'age

--- app/Http/Controllers/FakeController.php
+++ app/Http/Controllers/FakeController.php
@@ -4,9 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+require_once '../vendor/fzaninotto/faker/src/autoload.php';
+use Faker;
+
 class FakeController extends Controller
 {
-    public function getIndex(){
-    	return view('fake.index');
-    }
+	public function getIndex(){
+		$faker = Faker\Factory::create('fr_FR');
+		$dateFake = $faker->date($format = 'Y-m-d', $max = 'now');
+		$dateNow = date('Y-m-d');
+    
+		$datetimeFake = date_create($dateFake);
+		$datetimeNow = date_create($dateNow);
+		$interval = date_diff($datetimeFake, $datetimeNow);
+		$age = $interval->format('%y ans');
+		
+    	 return view('fake.index',['fakers'=>$faker,'dateFakes'=>$dateFake,'ages'=>$age]);
+	}
+
 }
