8466551c78010595e88755fb422e09537f00aaf3
Dylan <dylan4047@gmail.com>
2017-02-22 11:07:32 +0100

    ajout de la route pour l'acceuil

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('fake.index');
+// });
+Route::get('/','FakeController@getIndex');
