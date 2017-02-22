b95f7ad688e1f5281fb588a8ee542a815453d82d
Dylan <dylan4047@gmail.com>
2017-02-22 16:56:02 +0100

    transformation en json des data de Faker

--- app/Http/Controllers/FakeController.php
+++ app/Http/Controllers/FakeController.php
@@ -9,17 +9,45 @@ use Faker;
 
 class FakeController extends Controller
 {
+
+	public function __construct(){
+
+	}
 	public function getIndex(){
 		$faker = Faker\Factory::create('fr_FR');
+		$faker->seed();
 		$dateFake = $faker->date($format = 'Y-m-d', $max = 'now');
 		$dateNow = date('Y-m-d');
-    
+
 		$datetimeFake = date_create($dateFake);
 		$datetimeNow = date_create($dateNow);
 		$interval = date_diff($datetimeFake, $datetimeNow);
 		$age = $interval->format('%y ans');
 		
-    	 return view('fake.index',['fakers'=>$faker,'dateFakes'=>$dateFake,'ages'=>$age]);
+		// echo mt_srand((int) 242);
+		$fakeToJson = ['name'=>$faker->name,'address'=>$faker->address,'email'=>$faker->freeEmail,'birth'=>$dateFake,'old'=>$age,'text'=>$faker->text];
+		
+ 
+		$fakeJson =json_encode($fakeToJson);
+
+		// dd($fakeJson);
+
+		// require '../composer.json';
+		// var_dump($fakeJson);		
+		// $test = json_decode($fakeJson);
+		// dd($test);
+		$fileName = 'profil.json';
+		$testToJSon=fopen($fileName, 'w+');
+		fwrite($testToJSon, $fakeJson);
+		fclose($testToJSon);
+
+		
+		
+		
+		
+		
+		return view('fake.index',['fakers'=>$faker,'dateFakes'=>$dateFake,'ages'=>$age]);
 	}
+
 
 }
